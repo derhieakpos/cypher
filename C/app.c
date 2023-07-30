0866c94490099d68228302535e467daa57c47d31
derhieakpos <derhieakpos@gmail.com>
2023-07-30 18:20:56 +0100

    Work in progress

---
+++ C/app.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ */
+int main(void)
+{
+	char alphabets[a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+	int i;
+	char name[];
+
+	scanf("Enter input name: %s", name);
+	i = sizeof(name);
+	printf("size of name = %d", i);
+
+	//for (
---
+++ README.md
@@ -0,0 +1,2 @@
+This project creates cyphers by converting password alphabets to alpha-numeric keys
+The project will be written in C, Javascript and Python
