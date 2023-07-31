65907a2f5e9a866b15a80d89cd5d8f105de624ea
derhieakpos <derhieakpos@gmail.com>
2023-07-31 06:20:16 +0100

    Trying JavaScript first

--- C/app.c
+++ C/app.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char alphabets[a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
-	int i;
-	char name[];
+	// char alphabets["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+	// int i;
+	char name[0];
 
 	scanf("Enter input name: %s", name);
-	i = sizeof(name);
-	printf("size of name = %d", i);
+	// unsigned i = sizeof(name);
+	// printf("Name = %s", name);
 
 	//for (
